e901b9945ea6850035fdff53e5314c03bcbe371e
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-09-09 09:55:42 +0200

    Create alias.cmd

---
+++ user/.dotfiles/config/cmd/alias.cmd
@@ -0,0 +1,22 @@
+@echo off
+:: https://github.com/excalith/.dotfiles/blob/main/config/cmd/alias.cmd
+:: Commands
+
+DOSKEY ls=dir /B $*
+DOSKEY mkcd="%USERPROFILE%\.dotfiles\config\cmd\mkcd.bat" $*
+DOSKEY touch="%USERPROFILE%\.dotfiles\config\cmd\touch.bat" $*
+DOSKEY clear=cls
+DOSKEY cat=bat $*
+DOSKEY nano=micro $*
+
+:: Common directories
+
+DOSKEY dotfiles=cd "%USERPROFILE%\.dotfiles\$*"
+
+  
+:: Easy navigation
+
+DOSKEY ..=cd ..
+DOSKEY ...=cd ../..
+DOSKEY ....=cd ../../..
+DOSKEY .....=cd ../../../..
