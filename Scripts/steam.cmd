b90c91fd43a45fc00c9eecf16cf0f14f7c2ef100
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-15 07:37:18 +0100

    Create steam.cmd

---
+++ Scripts/steam.cmd
@@ -0,0 +1,4 @@
+
+
+:: steam launch options
+start Steam.exe -silent -quicklogin -forceservice -vrdisable -noshaders -nodirectcomp -cef-disable-gpu -cef-disable-gpu-sandbox -cef-allow-browser-underlay -cef-delaypageload -cef-force-occlusion -cef-disable-hang-timeouts -console"
