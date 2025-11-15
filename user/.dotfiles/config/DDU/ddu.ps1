8fc173f8accc0e114e8ed26a36697f05bd5044f3
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-11-15 08:23:30 +0100

    Create ddu.ps1

---
+++ user/.dotfiles/config/DDU/ddu.ps1
@@ -0,0 +1,7 @@
+
+
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\DriverSearching" /v "SearchOrderConfig" /t REG_DWORD /d "0" /f | Out-Null
+:: toggle safe mode
+cmd /c "bcdedit /set {current} safeboot minimal >nul 2>&1"
+:: restart
+shutdown -r -t 00
