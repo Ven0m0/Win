4011eb6aabe60075fb0d8390f99c565e42d6a663
Ven0m0 <ven0m0.wastaken@gmail.com>
2025-12-29 23:00:58 +0100

    Update add-contexmenu.bat

--- user/.dotfiles/config/nvidia/nvcpl/add-contexmenu.bat
+++ user/.dotfiles/config/nvidia/nvcpl/add-contexmenu.bat
@@ -4,6 +4,7 @@ dism >nul 2>&1 || (echo ^<Run the Script in Administrator^> && pause>nul && cls&
 echo info: add to context-menu
 reg add "HKCR\Directory\Background\shell\nvcpl" /v "MUIVerb" /t reg_sz /d "Nvidia Control Panel (Portable)" /f >nul
 reg add "HKCR\Directory\Background\shell\nvcpl" /v "Icon" /t reg_sz /d "%~dp0nvcpl.exe,6" /f >nul
-reg add "HKCR\Directory\Background\shell\nvcpl\command" /ve /d "\"C:\Windows\System32\WScript.exe\" \"%~dp0Nvidia Control Panel.vbs\"" /f >nul
+reg add "HKCR\Directory\Background\shell\nvcpl\command" /ve /d "\"C:\Windows\System32\WScript.exe\" \"%~dp0nvcpl.vbs\"" /f >nul
 pause
+
 
