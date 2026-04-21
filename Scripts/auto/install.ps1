f4e29e86195c5ee1550bf4c9913c8132f959134e
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-04-21 23:10:10 +0200

    Create install.ps1

---
+++ Scripts/auto/install.ps1
@@ -0,0 +1,14 @@
+
+
+winget install --id Git.Git -e --silent
+
+
+scoop install aria2
+scoop config aria2-enabled true
+
+dism /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
+schtasks /create /tn "post-reboot" /sc onlogon /rl highest /tr "powershell -ExecutionPolicy Bypass -File C:\setup\wsl-stage2.ps1"
+
+shutdown /r /t 0
