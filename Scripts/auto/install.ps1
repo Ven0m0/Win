e954c165dabe6e93b4f349329358c6da0c33a4d6
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-04-21 23:52:07 +0200

    Update install.ps1

--- Scripts/auto/install.ps1
+++ Scripts/auto/install.ps1
@@ -2,19 +2,21 @@
 ﻿#Requires -Version 5.1
 #Requires -RunAsAdministrator
 
-winget install -h Git.Git aria2.aria2 7zip.7zip
-winget install -h Microsoft.PowerShell Microsoft.WindowsTerminal Microsoft.AppInstaller 
+winget install --id Git.Git --source winget -h
+winget install --id Microsoft.PowerShell --source winget -h
+winget install -h Microsoft.WindowsTerminal Microsoft.AppInstaller 
 winget install -h Chocolatey.Chocolatey topgrade-rs.topgrade OpenJS.NodeJS Schniz.fnm Oven-sh.Bun Python.Python.3.13 astral-sh.uv jdx.mise 
-winget install -h Microsoft.DirectX Gyan.FFmpeg
+winget install -h Microsoft.DirectX Gyan.FFmpeg aria2.aria2 7zip.7zip
 
 winget insatall -h abbodi1406.vcredist Microsoft.VCLibs.14 Microsoft.VCLibs.Desktop.14 Microsoft.WindowsAppRuntime.1.8  
 winget install -h Microsoft.DotNet.Framework.Runtime Microsoft.DotNet.DesktopRuntime.5 Microsoft.DotNet.DesktopRuntime.6 Microsoft.DotNet.DesktopRuntime.7 Microsoft.DotNet.DesktopRuntime.8 Microsoft.DotNet.DesktopRuntime.9 Microsoft.DotNet.DesktopRuntime.10
-
 
 scoop config aria2-enabled true
 
 dism /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 dism /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+dism /online /enable-feature /featurename:LegacyComponents /all /norestart
+dism /online /enable-feature /featurename:DirectPlay /all /norestart
 
 schtasks /create /tn "post-reboot" /sc onlogon /rl highest /tr "powershell -ExecutionPolicy Bypass -File C:\setup\stage2.ps1"
 
