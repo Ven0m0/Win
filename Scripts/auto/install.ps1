b7a8bfed434121240d873464d17cdecd003d3858
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-04-22 00:03:43 +0200

    Update install.ps1

--- Scripts/auto/install.ps1
+++ Scripts/auto/install.ps1
@@ -6,7 +6,7 @@ winget install --id Git.Git --source winget -h
 winget install --id Microsoft.PowerShell --source winget -h
 winget install -h Microsoft.WindowsTerminal Microsoft.AppInstaller 
 winget install -h Chocolatey.Chocolatey topgrade-rs.topgrade OpenJS.NodeJS Schniz.fnm Oven-sh.Bun Python.Python.3.13 astral-sh.uv jdx.mise 
-winget install -h Microsoft.DirectX Gyan.FFmpeg aria2.aria2 7zip.7zip
+winget install -h Microsoft.DirectX Gyan.FFmpeg aria2.aria2 7zip.7zip KhronosGroup.VulkanRT Microsoft.OpenCLGLVulkanCompatibilityPack 
 
 winget insatall -h abbodi1406.vcredist Microsoft.VCLibs.14 Microsoft.VCLibs.Desktop.14 Microsoft.WindowsAppRuntime.1.8  
 winget install -h Microsoft.DotNet.Framework.Runtime Microsoft.DotNet.DesktopRuntime.5 Microsoft.DotNet.DesktopRuntime.6 Microsoft.DotNet.DesktopRuntime.7 Microsoft.DotNet.DesktopRuntime.8 Microsoft.DotNet.DesktopRuntime.9 Microsoft.DotNet.DesktopRuntime.10
