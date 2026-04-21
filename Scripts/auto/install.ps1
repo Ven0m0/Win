259ea0040e3f4841fcc17a5de7fa012175f27030
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-04-21 23:35:40 +0200

    Update install.ps1

--- Scripts/auto/install.ps1
+++ Scripts/auto/install.ps1
@@ -2,12 +2,15 @@
 ﻿#Requires -Version 5.1
 #Requires -RunAsAdministrator
 
-winget install --id Git.Git -e -h
-winget insatall -h abbodi1406.vcredist 
-winget install -h Microsoft.DotNet.Framework.Runtime Microsoft.DotNet.DesktopRuntime.5 Microsoft.DotNet.DesktopRuntime.6 Microsoft.DotNet.DesktopRuntime.7 Microsoft.DotNet.DesktopRuntime.8 Microsoft.DotNet.DesktopRuntime.9 \
-  Microsoft.DotNet.DesktopRuntime.10
+winget install -h Git.Git aria2.aria2 7zip.7zip
+winget install -h Microsoft.PowerShell Microsoft.WindowsTerminal Microsoft.AppInstaller 
+winget install -h Chocolatey.Chocolatey topgrade-rs.topgrade OpenJS.NodeJS Schniz.fnm Oven-sh.Bun Python.Python.3.13 astral-sh.uv jdx.mise 
+winget install -h Microsoft.DirectX Gyan.FFmpeg
 
-scoop install aria2
+winget insatall -h abbodi1406.vcredist Microsoft.VCLibs.14 Microsoft.VCLibs.Desktop.14 Microsoft.WindowsAppRuntime.1.8  
+winget install -h Microsoft.DotNet.Framework.Runtime Microsoft.DotNet.DesktopRuntime.5 Microsoft.DotNet.DesktopRuntime.6 Microsoft.DotNet.DesktopRuntime.7 Microsoft.DotNet.DesktopRuntime.8 Microsoft.DotNet.DesktopRuntime.9 Microsoft.DotNet.DesktopRuntime.10
+
+
 scoop config aria2-enabled true
 
 dism /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
