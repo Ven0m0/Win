7e52f1ba95c2409818485f45da5a0e97d85bfd75
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-04-25 23:30:06 +0200

    chore: remove temporary linting files

--- fix_format.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Remove trailing whitespace
-find . -name "*.ps1" -exec sed -i 's/[[:space:]]*$//' {} +
-# Replace tabs with 2 spaces
-find . -name "*.ps1" -exec sed -i 's/\t/  /g' {} +
