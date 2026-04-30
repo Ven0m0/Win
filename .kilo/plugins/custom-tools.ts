73b1ee52b32beeb3ca2277f5a5675028303b83b1
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-04-30 05:01:57 +0200

    Update custom-tools.ts

--- .kilo/plugins/custom-tools.ts
+++ .kilo/plugins/custom-tools.ts
@@ -2,7 +2,6 @@ import type { Plugin } from "@opencode-ai/plugin";
 import jsonRepair from "../tools/json_repair";
 import hlEdit from "../tools/hashline_edit";
 import { read as hlRead, grep as hlGrep } from "../tools/hashline_rg";
-import { search as sgSearch, replace as sgReplace } from "../tools/ast_grep";
 
 const CustomToolsPlugin: Plugin = async () => ({
   tool: {
@@ -10,8 +9,6 @@ const CustomToolsPlugin: Plugin = async () => ({
     hl_edit: hlEdit,
     hl_read: hlRead,
     hl_grep: hlGrep,
-    sg: sgSearch,
-    sgr: sgReplace,
   },
 });
 
