ffc8a70e0bc7fcd36b473452f5de4369cea0ec6d
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-05-03 09:30:42 +0200

    Create priority.ps1

---
+++ Scripts/reg/priority.ps1
@@ -0,0 +1,11 @@
+
+
+; === QoS PowerShell equivalent ===
+New-NetQosPolicy -Name "ArcRaiders" -AppPathNameMatchCondition "PioneerGame.exe" -DSCPAction 46
+New-NetQosPolicy -Name "BlackOps6" -AppPathNameMatchCondition "cod24-cod.exe" -DSCPAction 46
+New-NetQosPolicy -Name "Fortnite" -AppPathNameMatchCondition "FortniteClient-Win64-Shipping.exe" -DSCPAction 46
+gpupdate /force
+
+; === Windows Defender Exclusions ===
+Add-MpPreference -ExclusionProcess "node.exe","clang.exe","rustc.exe","cargo.exe","bun.exe","bunx.exe","sccache.exe"
+Add-MpPreference -ExclusionProcess "PioneerGame.exe","cod24-cod.exe","FortniteClient-Win64-Shipping.exe"
